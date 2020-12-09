cbacd542313d65656d8209bc38f6f5f604fe51c9
rakshith2001 <51781022+rakshith2001@users.noreply.github.com>
2020-12-09 19:21:37 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Mex9QbQHYxPxZBeEIsm0OHcGzdfcRMN6'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-LyDm8ZLjDrWoqXIOnA_5pEK8DGmkuwY'
 export_file_name = 'export.pkl'
 
 classes = ['Normal', 'Pneumonia']
